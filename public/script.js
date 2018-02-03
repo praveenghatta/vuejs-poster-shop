15317887364f02480a0d7cac848a840038547d51
Praveen Ghatta <praveenghatta@gmail.com>
2018-02-03 14:08:41 +0530

    Added Page scroll to the list

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
+
 var PRICE=9.99;
+var LOAD_NUM = 10;
 
 new Vue({
     el : '#app',
     data : {
         total : 0,
+        results : [],
         items : [],
         cart : [],
         newSearch : 'anime',
@@ -12,6 +15,12 @@ new Vue({
         price : PRICE
     },
     methods :{
+        appendItems : function(){
+            if(this.items.length < this.results.length){
+                var append = this.results.slice(this.items.length, this.items.length+LOAD_NUM);
+                this.items = this.items.concat(append);
+            }
+        },
         onSubmit:function(){
             this.loading = true;
             this.items = [];
@@ -19,7 +28,8 @@ new Vue({
                 .get('/search/'.concat(this.newSearch))
                 .then(function(res){
                     this.lastSearch = this.newSearch;
-                    this.items = res.data;
+                    this.results = res.data;
+                    this.appendItems();
                     this.loading = false;
                 })
             ;
@@ -70,5 +80,13 @@ new Vue({
 
     mounted : function(){
         this.onSubmit();
+
+        var vueInstance = this;
+        var elem = document.getElementById('product-list-bottom');
+        var watcher = scrollMonitor.create(elem);
+        watcher.enterViewport(function(){
+            vueInstance.appendItems();
+        });
     }
 });
+
